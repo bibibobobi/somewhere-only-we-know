8359fcd50f12913c1470db906405e3b5821e41e2
bibibobobi <smilingcat33@gmail.com>
2024-03-03 14:57:07 +0800

    feat(blog): optimize the hero section image

--- app/(BlogClient)/_components/neon-title.tsx
+++ app/(BlogClient)/_components/neon-title.tsx
@@ -1,17 +1,36 @@
+"use client";
 import { Marck_Script } from "next/font/google";
+import Image from "next/image";
+import { useState } from "react";
 const font = Marck_Script({ subsets: ["latin"], weight: ["400"] });
 
 export default function NeonTitle() {
+  const [loading, setLoading] = useState(true);
+
   return (
-    <div
-      className="bg-cover bg-center w-full flex items-center justify-center md:px-8 py-20 md:py-40 text-center"
-      style={{ backgroundImage: `url("/images/night.jpg")` }}
-    >
-      <h1
-        className={`${font.className} neonText  text-5xl md:text-6xl select-none`}
-      >
-        Somewhere Only We Know
-      </h1>
+    <div className="relative w-full h-[300px] md:h-[450px]">
+      {loading && (
+        <div className="absolute z-10 inset-0 bg-zinc-500 bg-opacity-30 blur-sm flex items-center justify-center pointer-events-none" />
+      )}
+      <Image
+        src="/images/night.jpg"
+        alt="Background image"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        priority={true}
+        onLoad={() => setLoading(false)}
+        onError={() => setLoading(false)}
+      />
+      {!loading && (
+        <div className="absolute inset-0 flex items-center justify-center px-4 md:px-8 text-center">
+          <h1
+            className={`${font.className} neonText text-5xl md:text-6xl select-none`}
+          >
+            Somewhere Only We Know
+          </h1>
+        </div>
+      )}
     </div>
   );
 }
